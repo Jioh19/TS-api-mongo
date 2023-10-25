52aaea85d66b3def6eef7c331665afd1ae5a6588
Jioh19 <jioh1982@gmail.com>
2023-10-25 17:09:12 -0300

    add controllers

---
+++ src/book/book.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schemas';
+
+@Controller('books')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async getAllBooks(): Promise<Book[]> {
+    return this.bookService.findAll();
+  }
+
+  @Post()
+  async createBook(
+    @Body()
+    book
+  ): Promise<Book> {
+    return this.bookService.create(book)
+  }
+}
